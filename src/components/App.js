4232d3cdada8d37345982fdc6a900c350ccbf046
casinca4 <s.antonelli@web.de>
2019-12-17 11:17:45 +0100

    Add all components

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/App.scss';
+import Navigation from './Navigation';
+import MainContainer from './MainContainer';
+
+function App() {
+  return (
+    <div className="app">
+      <Navigation></Navigation>
+      <MainContainer></MainContainer>
+    </div>
+  );
+}
+
+export default App;
+
+
+// oder nur <Navigation/> 
