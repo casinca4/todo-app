2e40478a22de452de509b92c056bd8d0cb184806
casinca4 <s.antonelli@web.de>
2020-01-08 11:32:56 +0100

    Class components

--- src/components/ToDosContainer.js
+++ src/components/ToDosContainer.js
@@ -6,14 +6,14 @@ function ToDosContainer(props) {
   
     // so werden Listen gemacht
     const todoItems = data.map(el => {      // el wird noch nicht benutzt
-        return <ToDoItem key={el.id} text={el.text} status={el.status}></ToDoItem>           // nicht mehr $
+        return <ToDoItem key={el.id} text={el.text} status={el.status}></ToDoItem>           
     });
 
     console.log(todoItems);
 
     return (
         <div className="todos-container">
-            {data.length > 0 && (               // wenn wahr, dann zeigt TODOS an; wenn nicht, dann nichts anzeigen
+            {data.length > 0 && (               // wenn wahr, dann zeigt TODOS an; wenn nicht, dann nichts anzeigen; si hay data, dann zeigt er TODOS an; so ist die Schreibweise
                 <div className="todos">
                     <h5>TODOS</h5>
                     {/* <ToDoItem></ToDoItem>   // ist eins von den vier items, gibt sie automatisch wieder */}
