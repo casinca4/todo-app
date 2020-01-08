b099babaa7109b0d2415fc5bb8fdb6321c82c194
casinca4 <s.antonelli@web.de>
2020-01-08 11:44:09 +0100

    Form set up

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,16 +1,58 @@
 import React from 'react';
 
-function FormContainer() {
-  return (                            // man muß immer was return
-    <div className="form-container">
-      <form className="todo-form">
-        <label className="input-item">
-          <input type="text" name="todo" />
-        </label>
-        <input className="btn" type="submit" value="ADD" />
-      </form>
-    </div>
-  );
+
+
+class FormContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.addTodo(this.state.value);
+    this.setState({ value: '' });
+  }
+
+  render() {
+    return (
+      <div className="form-container">
+        <form className="todo-form" onSubmit={this.handleSubmit}>
+          <label className="input-item">
+            <input
+              type="text"
+              name="todo"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input className="btn" type="submit" value="ADD" />
+        </form>
+      </div>
+    );
+  }
 }
+
+
+
+// function FormContainer() {
+//   return (                            // man muß immer was return
+//     <div className="form-container">
+//       <form className="todo-form">
+//         <label className="input-item">
+//           <input type="text" name="todo" />
+//         </label>
+//         <input className="btn" type="submit" value="ADD" />
+//       </form>
+//     </div>
+//   );
+// }
 
 export default FormContainer;
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -3,29 +3,99 @@ import FormContainer from './FormContainer';
 import ToDosContainer from './ToDosContainer';
 import ToDonesContainer from './ToDonesContainer';
 
-function MainContainer() {
-  const data = [
-    { text: 'Do your thing', status: false, id: 1407892 },
-    { text: 'Be yourself', status: true, id: 1467892 },
-    { text: 'Explain something', status: false, id: 5436436 },
-    { text: 'Be a dog', status: true, id: 4363434 },
-    { text: 'Bite a tree', status: false, id: 1411892 },
-    { text: 'Plant a tree', status: false, id: 1117892 },
-    { text: 'Save the world', status: true, id: 5436222 },
-    { text: 'Achieve world peace', status: false, id: 4363333 }
-  ];
+// function MainContainer() {
+//   const data = [
+//     { text: 'Do your thing', status: false, id: 1407892 },
+//     { text: 'Be yourself', status: true, id: 1467892 },
+//     { text: 'Explain something', status: false, id: 5436436 },
+//     { text: 'Be a dog', status: true, id: 4363434 },
+//     { text: 'Bite a tree', status: false, id: 1411892 },
+//     { text: 'Plant a tree', status: false, id: 1117892 },
+//     { text: 'Save the world', status: true, id: 5436222 },
+//     { text: 'Achieve world peace', status: false, id: 4363333 }
+//   ];
 
-  const todos = data.filter(el => !el.status);
-  const todones = data.filter(el => el.status);
-  // const todos = data.filter(el => {if (!el.status) return el;});
+//   const todos = data.filter(el => !el.status);
+//   const todones = data.filter(el => el.status); 
+//   // const todos = data.filter(el => {if (!el.status) return el;});
 
-  return (
-    <main className="main-container">
-      <FormContainer></FormContainer>
-      <ToDosContainer items={todos}></ToDosContainer>
-      <ToDonesContainer items={todones}></ToDonesContainer>
-    </main>
-  );
+//   return (
+//     <main className="main-container">
+//       <FormContainer></FormContainer>
+//       <ToDosContainer items={todos}></ToDosContainer>
+//       <ToDonesContainer items={todones}></ToDonesContainer>
+//     </main>
+//   );
+// }
+
+
+class MainContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { text: 'Do your thing', status: false, id: 1407892 },
+        { text: 'Be yourself', status: true, id: 1467892 },
+        { text: 'Explain something', status: false, id: 5436436 },
+        { text: 'Be a dog', status: true, id: 4363434 },
+        { text: 'Bite a tree', status: false, id: 1411892 },
+        { text: 'Plant a tree', status: false, id: 1117892 },
+        { text: 'Save the world', status: true, id: 5436222 },
+        { text: 'Achieve world peace', status: false, id: 4363333 }
+      ]
+    };
+  }
+
+  handleUpdate = id => {
+    const items = this.state.items;
+    const updatedItems = items.map(el => {
+      if (id === el.id) {
+        el.status = !el.status;
+      }
+
+      return el;
+    });
+
+    this.setState({ items: updatedItems });
+  };
+
+  handleAddTodo = value => {
+    const newItem = {
+      text: value,
+      status: false,
+      id: new Date().getTime()
+    };
+
+    this.setState({ items: [...this.state.items, newItem] });
+  };
+
+  render() {
+    const data = this.state.items;
+    const todos = data.filter(el => !el.status);
+    const todones = data.filter(el => el.status);
+    // const todos = data.filter(el => {if (!el.status) return el;});
+
+    return (
+      <main className="main-container">
+        <FormContainer addTodo={this.handleAddTodo}></FormContainer>
+        <ToDosContainer
+          items={todos}
+          updateFromChild={this.handleUpdate}
+        ></ToDosContainer>
+        <ToDonesContainer
+          items={todones}
+          updateFromChild={this.handleUpdate}
+        ></ToDonesContainer>
+      </main>
+    );
+  }
 }
 
+
 export default MainContainer;
+
+// updateFromChild: property
+
+// el !el: from true to false
+
+// status: false --> it's not done yet
